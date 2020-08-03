3533d2efaa25147396419e113d06729024597d60
Zac Klammer <zklamm@gmail.com>
2020-08-03 16:53:24 -0500

    Extract generateMultipleFunctionParams to own file in src/aws
    Co-authored-by: John Isom <John.Isom2001@gmail.com>
    Co-authored-by: Torrel Moseley <trrl.mo@hotmail.com>

--- bin/deploy.js
+++ bin/deploy.js
@@ -17,28 +17,6 @@ const lambdaRoleName = "lambda_basic_execution";
 const statesRoleName = "stepFunctions_basic_execution";
 const basenamesAndZipBuffers = getBasenamesAndZipBuffers();
 
-const generateFunctionParams = (basename, zipBuffer, role) => {
-  return {
-    Code: {
-      ZipFile: zipBuffer,
-    },
-    FunctionName: basename,
-    Handler: `${basename}.handler`,
-    Role: role.Role.Arn,
-    Runtime: "nodejs12.x",
-  };
-};
-
-const generateMultipleFunctionParams = async (
-  basenamesAndZipBuffers,
-  roleName
-) => {
-  const role = await iam.getRole({ RoleName: roleName }).promise();
-
-  return basenamesAndZipBuffers.map(({ basename, zipBuffer }) => {
-    return generateFunctionParams(basename, zipBuffer, role);
-  });
-};
 const createLambdaFunctions = (allParams) => {
   const createFunctionPromises = allParams.map((params) =>
     retryAsync(() => lambda.createFunction(params).promise(), 5, 7000, 0.6)
