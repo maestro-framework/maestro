a04972b6047af449fab67edb1fca440c55f37883
mo mac <trrl.mo@hotmail.com>
2020-08-05 14:23:21 -0600

    Read in 'region' in 'src/aws/services.js' from '~/.config/maestro/aws_account_info.json'

--- src/aws/services.js
+++ src/aws/services.js
@@ -1,7 +1,9 @@
+const fs = require('fs');
+const homedir = require('os').homedir();
 const AWS = require("aws-sdk");
 AWS.config.logger = console;
 const iam = new AWS.IAM();
-const region = "us-west-2";
+const region = JSON.parse(fs.readFileSync(homedir + '/.config/maestro/aws_account_info.json')).region;
 const apiVersion = "latest";
 const lambda = new AWS.Lambda({ apiVersion, region });
 const stepFunctions = new AWS.StepFunctions({ apiVersion, region });
