779399f1eadb3d7628c5a5f40eb7b519f8232077
Zac Klammer <zklamm@gmail.com>
2020-08-05 12:51:31 -0500

    Delete createIAMRole.js since it was renamed to establishIAMRole.js

--- src/aws/createIAMRole.js
+++
@@ -1,8 +0,0 @@
-const { iam } = require("./services");
-const generateRoleParams = require("./generateRoleParams");
-
-const createIAMRole = (roleName) => {
-  return iam.createRole(generateRoleParams(roleName)).promise();
-};
-
-module.exports = createIAMRole;
