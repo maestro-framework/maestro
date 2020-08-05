736aae112137d14dd22f4bfe8a96ca3f94ff36a1
mo mac <trrl.mo@hotmail.com>
2020-08-05 14:51:13 -0600

    Create function to retrieve the region from the config file

--- src/aws/generateStateMachineParams.js
+++ src/aws/generateStateMachineParams.js
@@ -1,11 +1,22 @@
 const { iam } = require('./services');
 const fs = require("fs");
+const os = require('os');
+const account_info_path = '/.config/maestro/aws_account_info.json';
+
+const retrieveRegion = (path) => {
+  const homedir = os.homedir();
+  const configFile = JSON.parse(fs.readFileSync(homedir + path));
+
+  return configFile.region;
+}
 
 const generateStateMachineParams = async (roleName, stateMachineName) => {
   const role = await iam.getRole({ RoleName: roleName }).promise();
   const definition = fs
     .readFileSync(`state-machines/${stateMachineName}.asl.json`)
     .toString();
+
+  definition.replace()
 
   return {
     definition,
