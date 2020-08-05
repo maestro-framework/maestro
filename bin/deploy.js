fad9fcee6667c242847399f4ec60658d37a171e3
Zac Klammer <zklamm@gmail.com>
2020-08-05 18:50:06 -0500

    Rewrite logic for attaching policies so that it only executes when a role is newly created

--- bin/deploy.js
+++ bin/deploy.js
@@ -1,24 +1,19 @@
 #!/usr/bin/env node
 
 // TODO: determine how to implement ../src/util/requireJSON.js
-const { lambdaPolicyArns, statesPolicyArns } = require("../src/config/policy-arn");
 const getBasenamesAndZipBuffers = require("../src/util/getBasenamesAndZipBuffers");
-const sleep = require("../src/util/sleep");
-const attachPolicies = require("../src/aws/attachPolicies");
-const generateMultipleFunctionParams = require('../src/aws/generateMultipleFunctionParams');
-const generateStateMachineParams = require('../src/aws/generateStateMachineParams');
-const establishIAMRole = require('../src/aws/establishIAMRole');
-const createLambdaFunctions = require('../src/aws/createLambdaFunctions');
-const createStepFunction = require('../src/aws/createStepFunction');
+const generateMultipleFunctionParams = require("../src/aws/generateMultipleFunctionParams");
+const generateStateMachineParams = require("../src/aws/generateStateMachineParams");
+const establishIAMRole = require("../src/aws/establishIAMRole");
+const createLambdaFunctions = require("../src/aws/createLambdaFunctions");
+const createStepFunction = require("../src/aws/createStepFunction");
 // TODO: Separate the retrieving of file basenames and creating zip buffers
 //   Specify files to retrieve by a workflow name
-const basenamesAndZipBuffers = getBasenamesAndZipBuffers('lambdas');
-const { lambdaRoleName, statesRoleName } = require('../src/config/roleNames');
-const stateMachineName = process.argv[2] || 'example-workflow'; // TODO: perhaps throw an error?
+const basenamesAndZipBuffers = getBasenamesAndZipBuffers("lambdas");
+const { lambdaRoleName, statesRoleName } = require("../src/config/roleNames");
+const stateMachineName = process.argv[2] || "example-workflow"; // TODO: perhaps throw an error?
 
 establishIAMRole(lambdaRoleName)
-  .then(() => attachPolicies(lambdaPolicyArns, lambdaRoleName))
-  .then(() => console.log("Successfully attached policies"))
   .then(() =>
     generateMultipleFunctionParams(basenamesAndZipBuffers, lambdaRoleName)
   )
@@ -26,8 +21,6 @@ establishIAMRole(lambdaRoleName)
   .then(() => console.log("Successfully created function(s)"));
 
 establishIAMRole(statesRoleName)
-  .then(() => attachPolicies(statesPolicyArns, statesRoleName))
-  .then(() => console.log("Successfully attached policies"))
   .then(() => generateStateMachineParams(statesRoleName, stateMachineName))
   .then(createStepFunction)
   .then(() => console.log("Successfully created state machine"))
--- src/aws/establishIAMRole.js
+++ src/aws/establishIAMRole.js
@@ -1,15 +1,46 @@
 const { iam } = require("./services");
-const sleep = require('../util/sleep');
+const { lambdaPolicyArns, statesPolicyArns } = require("../config/policy-arn");
+const sleep = require("../util/sleep");
+const retryAsync = require("../util/retryAsync");
 const generateRoleParams = require("./generateRoleParams");
 
+const attachPolicies = (policyArns, roleName) => {
+  const attachPolicyPromises = policyArns.map((policyArn) => {
+    const policyParams = {
+      PolicyArn: policyArn,
+      RoleName: roleName,
+    };
+
+    return retryAsync(
+      () => {
+        return iam.attachRolePolicy(policyParams).promise();
+      },
+      2,
+      1000
+    );
+  });
+
+  return Promise.all(attachPolicyPromises);
+};
+
+module.exports = attachPolicies;
+
 const establishIAMRole = async (roleName) => {
+  const policyArns = roleName.includes("lambda")
+    ? lambdaPolicyArns
+    : statesPolicyArns;
+
   try {
     return await iam.getRole({ RoleName: roleName }).promise();
-  } catch(err) {
+  } catch (err) {
     console.log(err.message);
     console.log(`Creating a new role called ${roleName}...`);
-    return await iam.createRole(generateRoleParams(roleName)).promise()
-    .then(() => sleep(7000));
+    return await iam
+      .createRole(generateRoleParams(roleName))
+      .promise()
+      .then(() => sleep(7000))
+      .then(() => attachPolicies(policyArns, roleName))
+      .then(() => console.log("Successfully attached policies"));
   } finally {
     console.log(`Successfully established a role called ${roleName}.`);
   }
