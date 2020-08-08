1c1c54847c6670d0f01258f8226fc178b10189f8
mo mac <trrl.mo@hotmail.com>
2020-08-07 18:30:08 -0600

    Create helper functions createHiddenMaestroDir and promptForAccountInfo in 'src/commands/config.js'

--- src/commands/config.js
+++ src/commands/config.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+const os = require('os');
+const promptAsync = require('../util/promptAsync');
+
+const createHiddenMaestroDir = () => {
+  const homedir = os.homedir();
+  const dir = '/.maestro';
+
+  if (!fs.existsSync(dir)){
+      fs.mkdirSync(homedir + dir);
+  }
+};
+
+const promptForAccountInfo = async () => {
+  const accountNum = await promptAsync('Please enter your AWS Account Number');
+  const reqion = await promptAsync('Please enter the region for you AWS servises');
+
+  return { accountNum, region };
+}
+
 const config = () => {
   
 };
