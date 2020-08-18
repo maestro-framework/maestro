81a52c3bd2607c53b4c36c9a52ef23323569cf64
mo mac <trrl.mo@hotmail.com>
2020-08-18 16:22:18 -0600

    Update src/util/awsAccountInfo.js to conditionally read hidden directory

--- src/util/awsAccountInfo.js
+++ src/util/awsAccountInfo.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 const configDir = require("./configDir.js");
+const hiddenAccountFilePath = `${configDir}"aws_account_info.json"`;
 
-const { accountNumber, region } = JSON.parse(
-  fs.readFileSync(`${configDir}/aws_account_info.json`)
-);
+let accountNumber;
+let region;
+
+if (fs.existsSync(hiddenAccountFilePath)) {
+  ({ accountNumber, region } = JSON.parse(
+    fs.readFileSync(hiddenAccountFilePath)
+  ));
+}
 
 module.exports = { accountNumber, region };
