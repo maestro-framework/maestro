e77953bbf33dbfa966b41cc0cb8a8b23f62d25a6
mo mac <trrl.mo@hotmail.com>
2020-08-20 13:47:21 -0600

    Test format of accountNumber as 12 digits

--- test/awsAccountInfo.test.js
+++ test/awsAccountInfo.test.js
@@ -1,10 +1,14 @@
 const assert = require("assert");
 const { accountNumber, region } = require("../src/util/awsAccountInfo.js");
 
-describe( "awsAccountInfo.js tests", () => {
+describe("awsAccountInfo.js tests", () => {
   describe("accountNumber Tests", () => {
-    it("accountNumber should be configured", ()=> {
+    const accountNumberFormatRegex = /\d{12}/;
+    it("accountNumber should be configured", () => {
       assert(accountNumber, "accountNumber is configured.");
     });
-  })
+    it("accountNumber should be 12 digits", () => {
+      assert.equal(accountNumberFormatRegex.test(accountNumber), true);
+    });
+  });
 });
