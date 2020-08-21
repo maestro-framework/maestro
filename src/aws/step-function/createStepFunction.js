e4012bd5a2eab3c943aacbff81a03383789196fc
Zac Klammer <zklamm@gmail.com>
2020-08-21 14:54:18 -0500

    Remove asl validation due to error in asl-validator dependency

--- src/aws/step-function/createStepFunction.js
+++ src/aws/step-function/createStepFunction.js
@@ -1,16 +1,7 @@
-const aslValidator = require("asl-validator");
 const { stepFunctions } = require("../services");
 
 const createStepFunction = (params) => {
-  const { definition } = params;
-  const { isValid, errorsText } = aslValidator(JSON.parse(definition));
-
-  if (isValid) {
-    return stepFunctions.createStateMachine(params).promise();
-  } else {
-    console.error("✕ State machine definition is invalid:", errorsText("\n"));
-    throw new Error();
-  }
+  return stepFunctions.createStateMachine(params).promise();
 };
 
 module.exports = createStepFunction;
