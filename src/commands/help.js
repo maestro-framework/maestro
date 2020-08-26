388446832d45fefaec76baceb087c78c9a38475d
John Isom <John.Isom2001@gmail.com>
2020-08-26 09:39:15 -0600

    Add placeholder help messages

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,39 @@
-const help = () => {};
+const genericHelpMsg  = "\"Generic help\" help message";
+const configMsg       = "\"Config\" help message";
+const deployMsg       = "\"Deploy\" help message";
+const teardownMsg     = "\"Teardown\" help message";
+const newMsg          = "\"New\" help message";
+const getTemplatesMsg = "\"Get templates\" help message";
+const helpMsg         = "\"Help\" help message";
+const defaultMsg      = "\"Default\" help message";
+
+const help = (argv) => {
+  switch (argv._[1]) {
+    case undefined:
+      console.log(genericHelpMsg);
+      break;
+    case "config":
+      console.log(configMsg);
+      break;
+    case "deploy":
+      console.log(deployMsg);
+      break;
+    case "teardown":
+      console.log(teardownMsg);
+      break;
+    case "new":
+      console.log(newMsg);
+      break;
+    case "get-templates":
+      console.log(getTemplatesMsg);
+      break;
+    case "help":
+      console.log(helpMsg);
+      break;
+    default:
+      console.log(defaultMsg);
+      break;
+  }
+};
 
 module.exports = help;
