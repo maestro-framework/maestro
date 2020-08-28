6f56c9de0ed4b6d0eb4c28e4a92082a42d3f6090
John Isom <John.Isom2001@gmail.com>
2020-08-28 17:09:07 -0600

    fill out default help message

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,11 +1,26 @@
-const genericHelpMsg  = "\"Generic help\" help message";
-const configMsg       = "\"Config\" help message";
-const deployMsg       = "\"Deploy\" help message";
-const teardownMsg     = "\"Teardown\" help message";
-const newMsg          = "\"New\" help message";
-const getTemplatesMsg = "\"Get templates\" help message";
-const helpMsg         = "\"Help\" help message";
-const defaultMsg      = "\"Default\" help message";
+const codes = {
+  reset:     "\033[m",
+  bold:      "\033[1m",
+  faint:     "\033[2m",
+  italic:    "\033[3m",
+  underline: "\033[4m",
+  red:       "\033[31m",
+  green:     "\033[32m",
+  yellow:    "\033[33m",
+  blue:      "\033[34m",
+  magenta:   "\033[35m",
+  cyan:      "\033[36m",
+  white:     "\033[37m",
+};
+
+const genericHelpMsg  = `Run \`${codes.blue}maestro help${codes.reset} ${codes.italic + codes.underline + codes.red}command${codes.reset}\` to get help specific to a subcommand.`;
+const configMsg       = `"Config" help message`;
+const deployMsg       = `"Deploy" help message`;
+const teardownMsg     = `"Teardown" help message`;
+const newMsg          = `"New" help message`;
+const getTemplatesMsg = `"Get templates" help message`;
+const helpMsg         = genericHelpMsg;
+const defaultMsg      = genericHelpMsg;
 
 const help = (argv) => {
   switch (argv._[1]) {
