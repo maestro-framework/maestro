7012b840b47c5e0a88819b71d3db1643a1ab77ca
John Isom <John.Isom2001@gmail.com>
2020-09-04 09:41:57 -0600

    Add option for verbose (was default) logging

--- src/util/logger.js
+++ src/util/logger.js
@@ -1,3 +1,16 @@
-const logger = () => {};
+const minimist = require("minimist");
+
+const isVerbose = minimist(process.argv.slice(2), {
+  boolean: ["verbose"],
+  alias: {
+    v: "verbose",
+  },
+}).verbose;
+
+const logger = (...args) => {
+  if (isVerbose) {
+    console.log(...args);
+  }
+};
 
 module.exports = logger;
